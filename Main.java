6a4f4a64c18af6307ba01b772bd302fc8095d066
patrickeharris <67847229+patrickeharris@users.noreply.github.com>
2020-07-04 23:30:15 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,6 +13,9 @@ public class Main
 {
   public static void main(String[] args) throws IOException { readSignsAnvil(); }
 
+
+
+  
   public static void readSignsAnvil() throws IOException {
     File folder = new File("region");
     File[] listOfFiles = folder.listFiles();
@@ -42,7 +45,7 @@ public class Main
               
               NBTTagCompound entity = nbttaglist.getCompoundTagAt(i);
               
-              if (entity.hasKey("Item") && entity.hasKey("Facing")) {
+              if (entity.hasKey("ArmorItems") && entity.hasKey("Pose")) {
                 String text1 = entity.getTag("Pos").toString();
                 text1 = text1.replaceAll("0:", "");
                 text1 = text1.replaceAll("1:", "");
@@ -53,21 +56,43 @@ public class Main
                 text1 = text1.substring(1, text1.lastIndexOf(","));
                 text1 = text1.replaceAll(",", ", ");
                 writer.write("Chunk [" + x + ", " + z + "]\t(" + text1 + ")\t");
-                String text2 = String.valueOf(entity.getCompoundTag("Item").getTag("id"));
+                String text2 = String.valueOf(entity.getTag("ArmorItems"));
+                text2 = text2.replaceAll("0:", "");
+                text2 = text2.replaceAll("1:", "");
+                text2 = text2.replaceAll("2:", "");
+                text2 = text2.replaceAll("3:", "");
+
+                
+                text2 = text2.substring(1, text2.lastIndexOf(","));
+                text2 = text2 + String.valueOf(entity.getTag("HandItems"));
+                text2 = text2.replaceAll("0:", "");
+                text2 = text2.replaceAll("1:", "");
+                text2 = text2.substring(0, text2.lastIndexOf(","));
+                text2 = text2.replace("}", "");
+                text2 = text2.replace("{", "");
+                text2 = text2.replace("[", "");
+                text2 = text2.replace("]", "");
+                text2 = text2.replaceAll("Count:1b", "");
+                text2 = text2.replaceAll("Damage:0s", "");
+                text2 = text2.replaceAll("id:", "");
                 text2 = text2.replaceAll("\"", "");
-                writer.write("Item: " + text2);
+                text2 = text2.replaceAll(",", "");
+                text2 = text2.replaceAll("minecraft", ", minecraft");
+                text2 = text2.replaceFirst(",", "");
+                writer.write("Items: " + text2);
+                
                 JSONObject json1 = null;
                 JSONObject json2 = null;
                 JSONObject json3 = null;
-                JSONObject json4 = null;                
-                JSONObject[] objects = { json1, json2, json3, json4 };  
+                JSONObject json4 = null;             
+                JSONObject[] objects = { json1, json2, json3, json4 };       
                 writer.newLine();
               } 
             } 
           } 
         } 
       } 
-    } 
+    }  
     writer.close();
   }
 }
